9d37551416b2cd149f629ade7c7d6742767f9633
Aydar Kanatov <aydar.kanatov@mail.ru>
2024-03-05 21:37:12 +0500

    /students endpoint base functionality

--- src/main/java/com/jilnash/courseproject/controller/StudentController.java
+++ src/main/java/com/jilnash/courseproject/controller/StudentController.java
@@ -1,33 +1,63 @@
 package com.jilnash.courseproject.controller;
 
 import com.jilnash.courseproject.dto.request.participants.StudentDTO;
+import com.jilnash.courseproject.dto.response.AppResponse;
+import com.jilnash.courseproject.service.StudentService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("students")
 public class StudentController {
 
+    @Autowired
+    private StudentService studentService;
+
     @GetMapping
-    public String getStudents() {
-        return "Students";
+    public ResponseEntity<?> getStudents() {
+        return ResponseEntity.ok(
+                new AppResponse(
+                        "List of students",
+                        200,
+                        studentService.getStudents()
+                )
+        );
     }
 
     @PutMapping
-    public boolean createStudent(@Valid @RequestBody StudentDTO studentDTO) {
-        System.out.println(studentDTO);
-        return true;
+    public ResponseEntity<?> createStudent(@Valid @RequestBody StudentDTO studentDTO) {
+        return ResponseEntity.ok(
+                new AppResponse(
+                        "Student created successfully",
+                        200,
+                        studentService.createStudent(studentDTO)
+                )
+        );
     }
 
     @GetMapping("{id}")
-    public String getStudent(@PathVariable Long id) {
-        return "Student: " + id;
+    public ResponseEntity<?> getStudent(@PathVariable Long id) {
+        return ResponseEntity.ok(
+                new AppResponse(
+                        "Student found successfully",
+                        200,
+                        studentService.getStudent(id)
+                )
+        );
     }
 
     @PatchMapping("{id}")
-    public boolean updateStudent(@PathVariable Long id,
+    public ResponseEntity<?> updateStudent(@PathVariable Long id,
                                  @Valid @RequestBody StudentDTO studentDTO) {
-        System.out.println(studentDTO);
-        return true;
+
+        return ResponseEntity.ok(
+                new AppResponse(
+                        "Student updated successfully",
+                        200,
+                        studentService.updateStudent(id, studentDTO)
+                )
+        );
     }
 }
--- src/main/java/com/jilnash/courseproject/service/StudentService.java
+++ src/main/java/com/jilnash/courseproject/service/StudentService.java
@@ -1,12 +1,53 @@
 package com.jilnash.courseproject.service;
 
+import com.jilnash.courseproject.dto.request.participants.StudentDTO;
+import com.jilnash.courseproject.model.participants.Student;
 import com.jilnash.courseproject.repo.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentService {
 
     @Autowired
     private StudentRepo studentRepo;
+
+    public List<Student> getStudents() {
+        return studentRepo.findAll();
+    }
+
+    public Student getStudent(Long id) {
+        return studentRepo
+                .findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException("Student not found"));
+    }
+
+    public Student createStudent(StudentDTO studentDTO) {
+
+        Student student = new Student();
+
+        student.setName(studentDTO.getName());
+        student.setSurname(studentDTO.getSurname());
+        student.setSkype(studentDTO.getSkype());
+
+        return studentRepo.save(student);
+    }
+
+    public boolean updateStudent(Long id, StudentDTO studentDTO) {
+
+        Student student = studentRepo
+                .findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException("Student not found"));
+
+        student.setName(studentDTO.getName());
+        student.setSurname(studentDTO.getSurname());
+        student.setSkype(studentDTO.getSkype());
+
+        studentRepo.save(student);
+
+        return true;
+    }
 }
